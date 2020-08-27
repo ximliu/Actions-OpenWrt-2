699e7fe90df264ba3aa4996c52a22d634f73d8ea
281677160 <66651997+281677160@users.noreply.github.com>
2020-08-27 16:50:57 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -46,6 +46,9 @@ svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/p
 
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 
+git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl  #Appliacation找到luci-app-k3screenctrl并勾选
+git clone https://github.com/lwz322/k3screenctrl_build.git package/lean/k3screenctrl_build  #k3screenctrl依赖
+
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
 #用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
