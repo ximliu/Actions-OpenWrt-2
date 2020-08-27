afc355e5c698fcf23f5c0918ef062710087ce03e
281677160 <66651997+281677160@users.noreply.github.com>
2020-08-27 22:12:02 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -54,5 +54,3 @@ rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
 #用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
 
-
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/infinityfreedom/d' package/lean/luci-theme-infinityfreedom/root/etc/uci-defaults/30_luci-theme-infinityfreedom
