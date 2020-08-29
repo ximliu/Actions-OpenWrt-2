d4f65b1cf68702b1df506137e6390ec51d273121
281677160 <66651997+281677160@users.noreply.github.com>
2020-08-30 00:57:08 +0800

    Update project-419.sh

--- project-419.sh
+++ project-419.sh
@@ -17,10 +17,5 @@ sed -i 's/OpenWrt/OpenWrt-123/g' ./package/base-files/files/bin/config_generate
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
 
 
-# 修改banne文件（banne文件在根目录）（不要修改此行代码,怕弄的diy-lede.sh文件全失效,不需要的话前面加#，或者全行代码删除了）
-rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./banner openwrt/package/base-files/files/etc/ && cd openwrt
-
-
-#内核版本是会随着源码更新而改变的，在project-openwrt/openwrt的源码查看最好，以X86机型为例，源码的target/linux/x86文件夹可以看到有几个内核版本，x86文件夹里Makefile可以查看源码正在使用内核版本
-#修改版本内核（代码前面有#为源码默认4.19内核,没#为4.9内核,还有一个4.14的，默认修改X86的，其他机型L大源码那里target/linux查看，对应修改下面的路径就好）
-#sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.9/g' ./target/linux/x86/Makefile  #修改内核版本
+#修改内核版本
+#sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.9/g' ./target/linux/x86/Makefile
