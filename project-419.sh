2876405189789b75e5d09e16fc395e0d2493f61e
281677160 <66651997+281677160@users.noreply.github.com>
2020-08-30 08:45:50 +0800

    Update project-419.sh

--- project-419.sh
+++ project-419.sh
@@ -23,7 +23,3 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/z
 
 # 删除已知的重复文件
 rm -rf ./package/lienol/luci-app-timecontrol
-
-
-#git clone https://github.com/lwz322/k3screenctrl.git package/ctcgfw/k3screenctrl  #k3屏幕依赖（其他路由器不需要）
-#sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3|' ./target/linux/bcm53xx/image/Makefile
