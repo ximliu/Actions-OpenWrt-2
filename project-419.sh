1df5affb9d0ab7ac02aa5fc1a8adbcc97eb4ed88
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-05 19:22:48 +0800

    Update project-419.sh

--- project-419.sh
+++ project-419.sh
@@ -29,7 +29,6 @@ rm -rf ./package/lienol/luci-app-timecontrol
 
 sed -i 's/BaiduPCS Web/百度网盘/g' ./package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
 sed -i 's/qBittorrent/BT下载/g' ./package/lean/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
-sed -i 's/aMule设置/电驴下载/g' ./package/lean/luci-app-amule/po/zh-cn/amule.po
 sed -i 's/网络存储/存储/g' ./package/lean/luci-app-amule/po/zh-cn/amule.po
 sed -i 's/Turbo ACC 网络加速/网络加速/g' ./package/lean/luci-app-turboacc/po/zh-cn/turboacc.po
 sed -i 's/带宽监控/网监/g' ./feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
