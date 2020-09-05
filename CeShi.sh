78d38f9c9e3e9b5e7d3f182203cb5a3e65ddbe69
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-05 19:17:21 +0800

    Update CeShi.sh

--- CeShi.sh
+++ CeShi.sh
@@ -55,5 +55,6 @@ sed -i 's/aMule设置/电驴下载/g' ./package/lean/luci-app-amule/po/zh-cn/amu
 sed -i 's/网络存储/存储/g' ./package/lean/luci-app-amule/po/zh-cn/amule.po
 sed -i 's/Turbo ACC 网络加速/网络加速/g' ./package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 sed -i 's/Turbo ACC 网络加速/网络加速/g' ./package/lean/luci-app-sfe/po/zh-cn/sfe.po
+sed -i 's/带宽监控/网监/g' ./feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 sed -i 's/带宽监控/网监/g' ./package/lean/default-settings/i18n/more.zh-cn.po
-sed -i 's/实时流量监测/流量/g' ./package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
+sed -i 's/实时流量监测/实时流量/g' ./package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
