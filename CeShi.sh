859feb025dafbb2cd5fcd640511a56479028046a
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-06 18:20:04 +0800

    Update CeShi.sh

--- CeShi.sh
+++ CeShi.sh
@@ -50,11 +50,9 @@ rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/
 #用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
 
 sed -i 's/BaiduPCS Web/百度网盘/g' ./package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
-sed -i 's/qBittorrent/BT下载/g' ./package/lean/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
+sed -i 's/cbi("qbittorrent"),_("qBittorrent")/cbi("qbittorrent"),_("BT下载")/g' ./package/lean/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
 sed -i 's/aMule设置/电驴下载/g' ./package/lean/luci-app-amule/po/zh-cn/amule.po
-sed -i 's/网络存储/存储/g' ./package/lean/luci-app-amule/po/zh-cn/amule.po
 sed -i 's/Turbo ACC 网络加速/网络加速/g' ./package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 sed -i 's/Turbo ACC 网络加速/网络加速/g' ./package/lean/luci-app-sfe/po/zh-cn/sfe.po
-sed -i 's/带宽监控/网监/g' ./feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
-sed -i 's/带宽监控/网监/g' ./package/lean/default-settings/i18n/more.zh-cn.po
+sed -i 's/带宽监控/监视/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 sed -i 's/实时流量监测/实时流量/g' ./package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
