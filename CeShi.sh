4c75fb3343ac97b7b6a9cf2379ad23f0fc950a55
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-08 09:48:40 +0800

    Update CeShi.sh

--- CeShi.sh
+++ CeShi.sh
@@ -56,3 +56,5 @@ sed -i 's/Turbo ACC 网络加速/网络加速/g' ./package/lean/luci-app-flowoff
 sed -i 's/Turbo ACC 网络加速/网络加速/g' ./package/lean/luci-app-sfe/po/zh-cn/sfe.po
 sed -i 's/带宽监控/监视/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 sed -i 's/实时流量监测/实时流量/g' ./package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
+
+sed -i "s/OpenWrt /P3TERX build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
