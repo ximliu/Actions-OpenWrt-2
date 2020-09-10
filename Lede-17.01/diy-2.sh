664796c12b827caf153b6fef2bc01be54ee23e4e
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-11 00:08:39 +0800

    Update diy-2.sh

--- Lede-17.01/diy-2.sh
+++ Lede-17.01/diy-2.sh
@@ -4,9 +4,6 @@ rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./Lede-17.01/banne
 # 修改argon为默认主题（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/OpenWrt /281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-
 #sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
 #sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
 
