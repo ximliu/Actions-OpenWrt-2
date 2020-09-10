66373fc50120b558137d6d1a13664f055a365679
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-10 13:50:44 +0800

    Update diy-2.sh

--- Project-18.06/diy-2.sh
+++ Project-18.06/diy-2.sh
@@ -1,9 +1,8 @@
-sed -i 's/BaiduPCS Web/百度网盘/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
+sed -i 's/BaiduPCS Web/百度网盘/g' package/ctcgfw/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
 sed -i 's/cbi("qbittorrent"),_("qBittorrent")/cbi("qbittorrent"),_("BT下载")/g' package/lean/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
 sed -i 's/aMule设置/电驴下载/g' package/lean/luci-app-amule/po/zh-cn/amule.po
 sed -i 's/网络存储/存储/g' package/lean/luci-app-amule/po/zh-cn/amule.po
-sed -i 's/Turbo ACC 网络加速/网络加速/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
-sed -i 's/Turbo ACC 网络加速/网络加速/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
+sed -i 's/Turbo ACC 网络加速/网络加速/g' package/lean/luci-app-turboacc/po/zh-cn/turboacc.po
 sed -i 's/实时流量监测/实时流量/g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
 sed -i 's/KMS 服务器/KMS激活/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
 sed -i 's/TTYD 终端/终端命令/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
