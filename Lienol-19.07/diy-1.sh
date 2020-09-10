2fcb5afa8cb98948febd1b73a960c184e7608788
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-10 23:14:35 +0800

    Update diy-1.sh

--- Lienol-19.07/diy-1.sh
+++ Lienol-19.07/diy-1.sh
@@ -12,9 +12,6 @@ sed -i 's/192.168.1.1/192.168.2.2/g' ./package/base-files/files/bin/config_gener
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/OpenWrt-123/g' ./package/base-files/files/bin/config_generate
 
-# 修改 argon 为默认主题（编译时候不选择那些会自动更改为默认主题的主题才有效）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
 # 内核显示增加自己个性名称（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些为后期增加）
 sed -i "s/OpenWrt /281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
 
