8696705e7c2be649937d35cf61ade33df7c96b99
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-10 18:50:16 +0800

    Update diy-1.sh

--- Lede-masetr/diy-1.sh
+++ Lede-masetr/diy-1.sh
@@ -11,7 +11,7 @@ sed -i 's/192.168.1.1/192.168.2.2/g' ./package/base-files/files/bin/config_gener
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/OpenWrt-123/g' ./package/base-files/files/bin/config_generate
 
-# 修改默认主题（不选择那些会自动改变为默认主题的主题才有效果）
+# 修改argon为默认主题（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
