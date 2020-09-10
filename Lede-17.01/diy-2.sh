66c2b662c458b3877c21d2cbe4b0dbf0e4b76e71
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-11 06:20:26 +0800

    Update diy-2.sh

--- Lede-17.01/diy-2.sh
+++ Lede-17.01/diy-2.sh
@@ -2,7 +2,7 @@
 rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./Lede-17.01/banner openwrt/package/base-files/files/etc/ && cd openwrt
 
 # 修改argon为默认主题（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 #sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.9/g' ./target/linux/x86/Makefile  #修改内核版本
 
