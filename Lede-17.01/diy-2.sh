3666b007d23def24df352960afc2697c1ddc6bec
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-11 00:12:06 +0800

    Update diy-2.sh

--- Lede-17.01/diy-2.sh
+++ Lede-17.01/diy-2.sh
@@ -4,8 +4,7 @@ rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./Lede-17.01/banne
 # 修改argon为默认主题（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-#sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
-#sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
+#sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=4.9/g' ./target/linux/x86/Makefile  #修改内核版本
 
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
 sed -i 's/"BaiduPCS Web"/"百度网盘"/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
