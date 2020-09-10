4b570f94dc74674ce0133d1df252f3b289719520
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-10 08:19:19 +0800

    Update diy-2.sh

--- x86-64/diy-2.sh
+++ x86-64/diy-2.sh
@@ -12,5 +12,8 @@ sed -i 's/USB 打印服务器/打印服务/g' package/lean/luci-app-usb-printer/
 sed -i 's/网络存储/存储/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
 sed -i 's/"Web 管理"/"页面管理"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
 
+sed -i 's/"管理权"/"修改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i 's/"系统"/"路由设置"/g' feeds/luci/modules/luci-mod-freifunk/po/zh-cn/freifunk.po
+
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i "s/OpenWrt /281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
