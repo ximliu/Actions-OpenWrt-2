fc48488ee9a648ead22678b70b37758f56f59ef6
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-11 11:04:52 +0800

    Update diy-2.sh

--- Lienol-19.07/diy-2.sh
+++ Lienol-19.07/diy-2.sh
@@ -1,5 +1,5 @@
 # 修改 banne 文件（banne在根目录的Lienol-19.07文件夹）
-#rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./Lienol-19.07/banner openwrt/package/base-files/files/etc/ && cd openwrt
+rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./Lienol-19.07/banner openwrt/package/base-files/files/etc/ && cd openwrt
 
 #sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
 
