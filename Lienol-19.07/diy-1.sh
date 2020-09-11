66309cda4a320eed4ee0f486202b9c96e0ab2f73
281677160 <66651997+281677160@users.noreply.github.com>
2020-09-11 15:39:57 +0800

    Update diy-1.sh

--- Lienol-19.07/diy-1.sh
+++ Lienol-19.07/diy-1.sh
@@ -3,7 +3,7 @@
 #
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/diy/luci-theme-edge  #主题-edge-动态登陆界面
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/diy/luci-theme-edge  #主题-edge
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/diy/luci-theme-infinityfreedom  #透明主题
 git clone -b master https://github.com/vernesong/OpenClash.git package/diy/luci-app-openclash  #openclash出国软件
 git clone https://github.com/frainzy1477/luci-app-clash.git package/diy/luci-app-clash  #clash出国软件
